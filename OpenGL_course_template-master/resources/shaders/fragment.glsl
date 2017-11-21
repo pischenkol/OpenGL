ad5ee25b87e6fc00abdd27ecc39b01297cc3c6e2
pischenkol <lina.pischenko@gmail.com>
2017-11-21 15:47:37 +0200

    added different colors for object

--- OpenGL_course_template-master/resources/shaders/fragment.glsl
+++ OpenGL_course_template-master/resources/shaders/fragment.glsl
@@ -1,8 +1,9 @@
 #version 330 core
 
-out vec4 color;
+in vec3 fragmentColor;
+out vec3 color;
 
 void main()
 {
-	color = vec4(1.0, 1.0, 1.0, 1.0);
+	color = fragmentColor;
 }
--- OpenGL_course_template-master/resources/shaders/vertex.glsl
+++ OpenGL_course_template-master/resources/shaders/vertex.glsl
@@ -1,7 +1,9 @@
 #version 330 core
 
 layout (location = 0) in vec3 position;
-
+layout(location = 1) in vec3 vertexColor;
+out vec3 fragmentColor;
 void main() {
     gl_Position = vec4(position, 1.0);
+    fragmentColor = vertexColor;
 }
